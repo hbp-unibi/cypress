d62b2848a43fe9bf5640deb9b61e0b20d3f88373
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-02-11 23:44:53 +0100

    Use proper argument parser in cli.py

--- python/cli.py
+++ python/cli.py
@@ -23,33 +23,118 @@ it. Writes the recorded data to the specified binnf file. Used as a interface
 between C++ code and PyNN.
 """
 
-import sys
-import os
+def do_run(args):
+    import binnf
+    import cypress
+    import sys
 
-import cypress
-import binnf
+    # Fetch the input/output file
+    in_filename = getattr(args, "in")
+    in_fd = sys.stdin if in_filename == '-' else open(in_filename, "rb")
+    out_filename = getattr(args, "out")
+    out_fd = sys.stdout if out_filename == '-' else open(out_filename, "rb")
 
-# Check the script parameters
-if len(sys.argv) != 4 and len(sys.argv) != 2:
-    print("Usage: ./binnf.py <SIMULATOR> [<IN> <OUT>]")
-    print("Where <IN> and <OUT> may be \"-\" to read from/write to "
-            + " stdin/stdout (default)")
-    sys.exit(1)
+    # Read the input data
+    network = binnf.read_network(in_fd)
 
-# Fetch the simulator name from the command line parameters
-simulator = sys.argv[1]
+    # Open the simulator and run the network
+    res = cypress.Cypress(
+        args.simulator,
+        args.library).run(
+        network,
+        duration=args.duration)
 
-# Fetch the input/output file
-input_file = (sys.stdin if (len(sys.argv) < 3
-        or sys.argv[2] == "-") else open(sys.argv[2], 'rb'))
-output_file = (sys.stdout if (len(sys.argv) < 4
-        or sys.argv[3] == "-") else open(sys.argv[3], 'wb'))
+    # Write the recorded data back to binnf
+    binnf.write_result(out_fd, res)
 
-# Read the input data
-network = binnf.read_network(input_file)
+def do_dump(args):
+    import binnf
+    import numpy as np
+    import sys
 
-# Open the simulator and run the network
-res = cypress.Cypress(simulator, "pyNN." + simulator).run(network, duration=1000.0)
+    # Set some numpy print options
+    np.set_printoptions(precision=3, suppress=True)
 
-print res
+    # Fetch the input file
+    in_filename = getattr(args, "in")
+    in_fd = sys.stdin if in_filename == '-' else open(in_filename, "rb")
+
+    # Dump the contents of the binnf stream
+    while True:
+        # Deserialise the input stream
+        name, header, matrix = binnf.deseralise(in_fd)
+        if name is None:
+            return
+
+        # Pretty-print the matrix
+        sys.stdout.write("== " + name + " ==\n\n")
+
+        # Generate the table columns
+        table = map(lambda x: [x["name"]], header)
+        for i in xrange(len(header)):
+            table[i] = table[i] + (map(str, matrix[header[i]["name"]]))
+
+        # Justify the columns
+        max_col_len = map(lambda x: max(map(len, x)), table)
+        table = map(lambda c_l: map(lambda s: " " *
+                                    (c_l[1] -
+                                     len(s)) +
+                                    s, c_l[0]), zip(table, max_col_len))
+
+        # Print the table
+        for i in xrange(len(matrix) + 1):
+            for j in xrange(len(header)):
+                if j > 0:
+                    sys.stdout.write(" | ")
+                sys.stdout.write(table[j][i])
+            sys.stdout.write("\n")
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+#
+# Parse the command line
+#
+import argparse
+parser = argparse.ArgumentParser(
+    description="Command line interface to the Python part of Cypress")
+sp = parser.add_subparsers()
+
+# Options for the "run" subcommand
+sp_run = sp.add_parser("run", help="Executes a neural network")
+sp_run.add_argument(
+    "--simulator",
+    type=str,
+    help="Canonical name of the simulator", required=True)
+sp_run.add_argument(
+    "--library",
+    type=str,
+    help="PyNN backend to use (e.g. pyNN.nest)", required=True)
+sp_run.add_argument(
+    "--duration",
+    type=float,
+    help="Duration of the simulation", required=True)
+sp_run.add_argument(
+    "--in",
+    type=str,
+    default="-",
+    help="Input binnf filename, use \"-\" for stdin")
+sp_run.add_argument(
+    "--out",
+    type=str,
+    default="-",
+    help="Output binnf filename, use \"-\" for stdin")
+sp_run.set_defaults(func=do_run)
+
+# Options for the "dump" subcommand
+sp_dump = sp.add_parser("dump", help="Decodes a binnf data stream")
+sp_dump.add_argument(
+    "--in",
+    type=str,
+    default="-",
+    help="Input binnf filename, use \"-\" for stdin")
+sp_dump.set_defaults(func=do_dump)
+
+# Parse the command line arguments and execute the associated function
+args = parser.parse_args()
+args.func(args)
 
