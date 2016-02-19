d9f5f3f27fb9b5176a37a4cf1ae4708fd5056eb1
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-02-19 14:00:46 +0100

    Cleanup CMakeLists

---
+++ examples/simple.cpp
@@ -0,0 +1,39 @@
+/*
+ *  Cypress -- C++ Spiking Neural Network Simulation Framework
+ *  Copyright (C) 2016  Andreas Stöckel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <fstream>
+
+#include <cypress/cypress.hpp>
+
+using namespace cypress;
+
+int main(int argc, const char *argv[]) {
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <SIMULATOR>" << std::endl;
+		return 1;
+	}
+	PyNN backend(argv[1]);
+	std::cout << backend.simulator() << std::endl;
+	std::cout << backend.normalised_simulator() << std::endl;
+	for (std::string simulator : backend.imports()) {
+		std::cout << simulator << std::endl;
+	}
+
+	Network().run(backend);
+}
