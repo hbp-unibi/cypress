18c62e8c53ea4e273296f3f78caa27de30b3915c
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-03-11 22:39:19 +0100

    Work around sPyNNaker issue #190

--- resource/backend/pynn/cypress.py
+++ resource/backend/pynn/cypress.py
@@ -235,7 +235,10 @@ class Cypress:
         is_source = type_name == TYPE_SOURCE
 
         # Create the population and setup recording
-        res = self.sim.Population(count, type_, {})
+        params = {}
+        if is_source and self.simulator == "nmmc1":
+            params = {"spike_times": []} # sPyNNaker issue #190
+        res = self.sim.Population(count, type_, params)
 
         # Bug fix for old PyHMF version
         if not hasattr(res, "size"):
