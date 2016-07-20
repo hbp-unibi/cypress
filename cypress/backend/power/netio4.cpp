67bc0de11ae29f7a307053ca7794ec2019c478ce
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-07-20 12:49:34 +0200

    Correct license header

--- cypress/backend/power/netio4.cpp
+++ cypress/backend/power/netio4.cpp
@@ -1,6 +1,6 @@
 /*
- *  CppNAM -- C++ Neural Associative Memory Simulator
- *  Copyright (C) 2016  Christoph Jenzen, Andreas Stöckel
+ *  Cypress -- C++ Spiking Neural Network Simulation Framework
+ *  Copyright (C) 2016  Andreas Stöckel
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
